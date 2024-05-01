00f34b6d64a26b9c32961b4dd05722b27dda1323
Kima4Tec <150744968+Kima4Tec@users.noreply.github.com>
2024-05-01 10:17:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInPraktik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Original value";
+            Console.WriteLine(myString);
+            ChangeInput(ref myString); 
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+        static void ChangeInput(ref string input)
+        {
+            input = "Changed value";
+        }
+    }
+}
